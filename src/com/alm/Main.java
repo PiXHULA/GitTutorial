5190ec20c7f2c08d2a73a1b45efa753442fb9d2d
Joakim Briceno <joakim.onnhage@gmail.com>
2020-08-18 15:48:27 +0200

    Trying to create a pull request from terminal

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -31,7 +31,7 @@ public class Main {
     }
 
     public static String getInput(String message) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print(message);
         return sc.nextLine();
     }
