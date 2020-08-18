28d1dfc021ea937f7501d2e097962beafc8f73a1
Joakim Briceno <joakim.onnhage@gmail.com>
2020-08-18 14:28:10 +0200

    Trying to create a conflict

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -3,6 +3,7 @@ package com.alm;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fazli");
+        String user = "Fazli";
+        System.out.println(user);
     }
 }
