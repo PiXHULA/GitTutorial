84466bbacce533c5817195cce02923f7dc1ec0b6
Joakim Briceno <joakim.onnhage@gmail.com>
2020-08-18 15:16:57 +0200

    Changed branch and added important code

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args){
 
-        String user = "Fazli";
+        String user = "Fazli kan inte så mkt";
         String secondUser = "Atef";
         System.out.println(user + " " + secondUser);
 
         user = "Jocke";
         System.out.println(user);
-
     }
 }
