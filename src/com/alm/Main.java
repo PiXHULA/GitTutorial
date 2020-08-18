45bfe6300b863dc2266241f4b26e4630d22a2edf
fazlizekiqi <fazlizekiqi1@hotmail.com>
2020-08-18 17:44:02 +0200

    pull request terminal

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
 
         String user = "Fazli kan inte så mkt";
-        String secondUser = "Atef + Jocke=<3";
+        String secondUser = "Atef";
         System.out.println(user + " " + secondUser);
 
         user = "Jocke lite bojke";
