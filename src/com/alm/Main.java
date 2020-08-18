df049e4e72bf1b52b5e47f1f650b5787c615c60b
Joakim Briceno <joakim.onnhage@gmail.com>
2020-08-18 15:13:18 +0200

    Added Atef as a second user

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         String user = "Fazli";
-        System.out.println(user);
+        String secondUser = "Atef";
+        System.out.println(user + " " + secondUser);
     }
 }
