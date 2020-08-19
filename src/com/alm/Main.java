8c29d872d7a82def3298b6e43538ec043e78cecc
jacobswenson <jacob.swenson@outlook.com>
2020-08-19 10:27:13 +0200

    created simple functionality

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -1,24 +1,30 @@
 package com.alm;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String[] gitMessage = {"Kontrollera vart nånstans användaren ligger? ", "Skapa en mapp med namnet 'softwaredev' för sitt första projekt",
+                "Gå in i mappen du skapade", "skapa filen Main.java", "Börja en initial commit",
+                "Kolla vilka filer vi har i mappen", "Lägg till alla filer i projektet", "Lägg till en fil i commiten", "Se tracked och untracked filer", "Commita med kommentar 'first commit'",
+                "Lägg till remote på: https://github.com/myfirstgithubrepo/softwaredev.git", "pusha till master"};
 
-        String user = "Fazli kan inte så mkt";
-        String secondUser = "Atef";
-        String thirdUser = "Jacob";
-        System.out.println(user);
-        System.out.println(secondUser);
-        System.out.println(thirdUser);
-
-        user = "Jocke lite bojke";
-        System.out.println(user);
-
-
-        String [] gitCommands = {"pwd", "mkdir dirName",
-                "cd softwaredev/", "touch Main.java", "git init" ,
+        String[] gitCommands = {"pwd", "mkdir softwaredev",
+                "cd softwaredev/", "touch Main.java", "git init",
                 "ls", "git add .", "git add Main2.java", "git status", "git commit -m \"first commit\"",
-        "git remote add origin https://github.com/myfirstgithubrepo/softwaredev.git", "git push -u origin master"};
+                "git remote add origin https://github.com/myfirstgithubrepo/softwaredev.git", "git push -u origin master"};
 
+        for (int i = 0; i < gitCommands.length; i++) {
+            while(!getInput(gitMessage[i] + ": ").equals(gitCommands[i])) {
+                System.out.println("Felaktig inmatning, försök igen");
+            }
+        }
     }
+    public static String getInput(String message) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print(message);
+        return sc.nextLine();
+    }
+
 }
