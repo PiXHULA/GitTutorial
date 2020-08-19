15affd78d2c4473e64a0e3574a63148dabb6b3c0
Kidboo10 <atef.sendesni@yh.nackademin.se>
2020-08-19 19:00:15 +0200

    fixing responses

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -7,17 +7,18 @@ public class Main {
     public static void main(String[] args) {
         String[] gitMessage = {"Kontrollera vart nånstans användaren ligger? ", "Skapa en mapp med namnet 'softwaredev' för sitt första projekt",
                 "Gå in i mappen du skapade", "skapa filen Main.java", "Börja en initial commit",
-                "Kolla vilka filer vi har i mappen", "Lägg till alla filer i projektet", "Lägg till en fil i commiten", "Se tracked och untracked filer", "Commita med kommentar 'first commit'",
+                "Kolla vilka filer vi har i mappen", "Lägg till alla filer i projektet", "Lägg till fil Main2.java i commiten", "Se tracked och untracked filer", "Commita med kommentar 'first commit'",
                 "Lägg till remote på: https://github.com/myfirstgithubrepo/softwaredev.git", "pusha till master"};
 
-
+        String[] gitAnswer = {"/c/Users/user", "softwaredev skapad", "/c/Users/user/softwaredev","Main.java skapad ", "ny Git repository skapad"
+        ,"Main.java\nMain2.java","Main.java added","Main2.java added","Main.java ready to commit\nMain2.java ready to commit"
+        ,"Main.java commit\nMain2.java commit\nKomentar: first commit","git remote origin added","push project remote to github\nGRATTIS"};
 
 
         String[] gitCommands = {"pwd", "mkdir softwaredev",
                 "cd softwaredev/", "touch Main.java", "git init",
                 "ls", "git add .", "git add Main2.java", "git status", "git commit -m \"first commit\"",
                 "git remote add origin https://github.com/myfirstgithubrepo/softwaredev.git", "git push -u origin master"};
-
 
 
 
@@ -32,5 +33,6 @@ public class Main {
         System.out.print(message);
         return sc.nextLine();
     }
+
 
 }
