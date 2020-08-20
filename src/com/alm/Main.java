46684f52826c424c164f957db52dfc4e363b1100
fazlizekiqi <fazlizekiqi1@hotmail.com>
2020-08-20 18:21:57 +0200

    fixing swing

--- src/com/alm/Main.java
+++ src/com/alm/Main.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 
 public class Main {
-    static String mac = "Mac OS X";
-    static String windows = "Windows";
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        new Controller();
     }
 }
 
--- src/com/alm/View.java
+++ src/com/alm/View.java
@@ -1,17 +1,35 @@
 package com.alm;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
+import static java.awt.Color.BLACK;
+import static javax.swing.JScrollPane.HORIZONTAL_SCROLLBAR_NEVER;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-public class View extends JFrame{
+public class View extends JFrame {
 
     Question currentQuestion;
     Controller controller;
@@ -21,11 +39,13 @@ public class View extends JFrame{
     JTextArea textArea;
     JLabel label;
     OS OS;
+    ProcessBuilder processBuilder;
 
-    public View(Question startingQuestion,Controller controller) {
+    public View(Question startingQuestion, Controller controller) {
 
         OS = OSValidator.getOs();
-
+        processBuilder = new ProcessBuilder();
+        String username = getUsername("whoami").get(0);
         currentQuestion = startingQuestion;
         this.controller = controller;
 
@@ -38,87 +58,78 @@ public class View extends JFrame{
         jLabel.setFont(new Font("Calibri", Font.PLAIN, 72));
         jLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-
         description = new JLabel(currentQuestion.getMessage(), SwingConstants.CENTER);
         description.setFont(new Font("Calibri", Font.PLAIN, 12));
-        description.setBorder(new EmptyBorder(20,0,20,0));
+        description.setBorder(new EmptyBorder(20, 0, 20, 0));
 
         //input output panels
+
         JPanel terminal = new JPanel(new BorderLayout());//inputpanel och outpanel
-//        JPanel inputPanel = new JPanel(new FlowLayout()); //label(t) och textfield
+        terminal.setBackground(BLACK);
 
         //containers
         JPanel inputPanel = new JPanel();
-        inputPanel.setSize(100,20);
-        JPanel outpanel =  new JPanel();
+        inputPanel.setMaximumSize(new Dimension(100, 20));
 
+        inputPanel.setBackground(BLACK);
 
+        JPanel outpanel = new JPanel();
+        outpanel.setBackground(BLACK);
 
-        inputPanel.setBackground(Color.BLACK);
-        String username = getUsername("whoamI").get(0);
         label = new JLabel(username + "$ ", SwingConstants.LEFT);
         label.setForeground(Color.WHITE);
-        label.setBackground(Color.RED);
 
         textField = new JTextField();
-        textField.setBackground(Color.BLACK);
-        textField.setForeground(Color.white);
-        textField.setBorder(new EmptyBorder(0,0,0,0));
-        textField.setPreferredSize(new Dimension(100,20));
 
-        textArea = new JTextArea(5,1);
-        textArea.setBackground(Color.BLACK);
+        textField.setBackground(BLACK);
+        textField.setForeground(Color.white);
+        textField.setBorder(new EmptyBorder(0, 0, 0, 0));
+        textField.setPreferredSize(new Dimension(400, 20));
+        textArea = new JTextArea(20, 45);
+        JScrollPane scroll = new JScrollPane(textArea, VERTICAL_SCROLLBAR_ALWAYS, HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollBar verticalScrollBar = new JScrollBar();
+        verticalScrollBar.setBackground(BLACK);
+        scroll.setVerticalScrollBar(verticalScrollBar);
+        scroll.setBackground(BLACK);
+
+        textArea.setBackground(BLACK);
         textArea.setForeground(Color.WHITE);
-        textArea.setSize(500,500);
+        textArea.setSize(500, 500);
         textArea.setEditable(false);
         textArea.setLineWrap(true);
-
 
         inputPanel.add(label);
         inputPanel.add(textField);
 
-        outpanel.add(textArea);
+        outpanel.add(scroll);
 
-        terminal.setBackground(Color.BLACK);
-        terminal.add(inputPanel,BorderLayout.LINE_START);
-        terminal.add(outpanel,BorderLayout.PAGE_END);
+        terminal.setBackground(BLACK);
+        terminal.add(inputPanel, BorderLayout.LINE_START);
+        terminal.add(outpanel, BorderLayout.PAGE_END);
 
-
-        textField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
+        textField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyChar()==KeyEvent.VK_ENTER) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
                     if (textField.getText().equalsIgnoreCase(currentQuestion.getCommand())) {
                         textArea.setText(getUsername(textField.getText()).stream().collect(Collectors.joining("\n")));
                         correct();
                     } else {
                         JOptionPane.showMessageDialog(null, "Fel, försök igen");
-                        System.out.println(textField.getText());
                         textField.setText("");
                     }
                 }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
             }
         });
 
         contentPanel.add(createImage(), BorderLayout.NORTH);
         contentPanel.add(terminal, BorderLayout.CENTER);
-        contentPanel.add(description,BorderLayout.SOUTH);
-        contentPanel.setBorder(new EmptyBorder(20,90,20,90));
+        contentPanel.add(description, BorderLayout.SOUTH);
+        contentPanel.setBorder(new EmptyBorder(20, 90, 20, 90));
 
         mainPanel.add(contentPanel);
 
         add(mainPanel);
-//        pack(); //SpringLayout
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
         setVisible(true);
@@ -130,8 +141,10 @@ public class View extends JFrame{
     }
 
     public void correct() {
-        if(textField.getText().equals("pwd"))
+        String inputText = textField.getText();
+        if (inputText.equals("pwd") || inputText.startsWith("cd")) {
             label.setText(currentQuestion.getOutput());
+        }
 
         controller.newQuestion();
         description.setText(currentQuestion.getMessage());
@@ -149,11 +162,9 @@ public class View extends JFrame{
         System.exit(0);
     }
 
-    public java.util.List<String> getUsername(String input){
+    public java.util.List<String> getUsername(String input) {
 
-        ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command(OS.SHELL, OS.C, input);
-
         try {
             Process process = processBuilder.start();
             InputStream inputStream = process.getInputStream();
